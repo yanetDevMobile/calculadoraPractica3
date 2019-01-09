ce91a7ddb72e4fcdb059e04d6edb76dbc75737a0
usuario <usuario@MacBook-Pro-de-07.local>
2019-01-09 14:38:05 -0600

    Initial Commit

---
+++ Practica 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica 3
+//
+//  Created by usuario on 1/9/19.
+//  Copyright © 2019 usuario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
