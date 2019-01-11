4140bd2a0406d452afd392ca0c94a6cf4ad4b07f
usuario <usuario@MacBook-Pro-de-07.local>
2019-01-11 08:19:22 -0600

    convertir a tipo de dato flotante

--- Practica 3/ViewController.swift
+++ Practica 3/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
  
-    @IBOutlet weak var propina: UITextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBOutlet weak var propinaTxt: UITextField!
     @IBOutlet weak var montoTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
 
+    @IBAction func calcularBtn(_ sender: UIButton) {
+        var monto : Float? = Float(self.montoTxt.text!)
+        var propina : Float? = Float(self.propinaTxt.text!)
+        var result : Float?
+        
+    }
 }
 // MARK: - Extensiones para PickerView
 
